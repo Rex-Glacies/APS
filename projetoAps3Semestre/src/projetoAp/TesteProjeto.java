54830f58bd84c7464152b94e4435281f59b7b4d3
Leonardo <Leonardo@BOOK-QISPLLPOC2>
2025-05-14 11:09:23 -0300

    Codigo alterado em sala

--- projetoAps3Semestre/src/projetoAp/TesteProjeto.java
+++ projetoAps3Semestre/src/projetoAp/TesteProjeto.java
@@ -19,8 +19,28 @@ public class TesteProjeto {
 		System.out.println("6 - Listar Notas");
 		System.out.println("0 - Sair");
 		System.out.print("Escolha uma opção: ");
-		opcao = Integer.parseInt(scanner.nextLine());
 
+		opcao = Integer.parseInt(scanner.nextLine());
+		
+		while (opcao > 6 || opcao < 0) {
+			System.out.println("Valor Invalido");
+			System.out.println("\n====== MENU DE CURSOS ======");
+			System.out.println("1 - Cadastrar novo curso");
+			System.out.println("2 - Listar cursos");
+			System.out.println("3 - Cadastrar Aluno");
+			System.out.println("4 - Listar Alunos");
+			System.out.println("5 - Cadastrar Nota");
+			System.out.println("6 - Listar Notas");
+			System.out.println("0 - Sair");
+			System.out.print("Escolha uma opção: ");
+						
+			try {
+				opcao = Integer.parseInt(scanner.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("Erro: a opção não é um número válido.");
+			}
+		}
+		
 		
 		switch (opcao) {
 			case 1:
@@ -32,7 +52,12 @@ public class TesteProjeto {
 				String nivel = scanner.nextLine();
 
 				System.out.print("Ano do curso: ");
-				int ano = Integer.parseInt(scanner.nextLine());
+				int ano = 0;
+				try {
+					 ano = Integer.parseInt(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 				
 				Curso ncurso = new Curso(nomeCurso, nivel, ano);
 				EscritaCSV.escreveCsvCurso(ncurso);
@@ -45,8 +70,12 @@ public class TesteProjeto {
 			case 3:
 				GerenciarArquivos.decidirArquivo(opcao);
 				System.out.println("Id do aluno: ");
-				int id = Integer.parseInt(scanner.nextLine());
-				
+				int id = 0;
+				try {
+					 id = Integer.parseInt(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 				System.out.println("Nome do aluno: ");
 				String nomeAluno = scanner.nextLine();
 
@@ -65,26 +94,56 @@ public class TesteProjeto {
 				String niCurso = scanner.nextLine();
 
 				System.out.println("Ano do curso para inserir a nota: ");
-				int anCurso = Integer.parseInt(scanner.nextLine());
+				int anCurso = 0;
+				try {
+					anCurso = Integer.parseInt(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 
 				GerenciarArquivos.decidirArquivoNota(opcao,nCurso, niCurso, anCurso);
 
 				System.out.println("Digite o Id do aluno: ");
-				int alunoid = Integer.parseInt(scanner.nextLine());
+				int alunoid = 0;
+				try {
+					alunoid = Integer.parseInt(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 
 				System.out.println("Digite a nota da NP1:");
-				float np1 = Float.parseFloat(scanner.nextLine());
+				float np1 = 0;
+				try {
+					np1 = Float.parseFloat(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 
 				System.out.println("Digite a nota da NP2:");
-				float np2 = Float.parseFloat(scanner.nextLine());
+				float np2 = 0;
+				try {
+					np2 = Float.parseFloat(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 
 				System.out.println("Digite a nota da Reposição:");
 				String reposicaoInput = scanner.nextLine();
-				float reposicao = reposicaoInput.isEmpty() ? -1 : Float.parseFloat(reposicaoInput);
+				float reposicao = 0;
+				try {
+					reposicao = reposicaoInput.isEmpty() ? -1 : Float.parseFloat(reposicaoInput);
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 
 				System.out.println("Digite a nota do exame:");
 				String exameInput = scanner.nextLine();
-				float exame = exameInput.isEmpty() ? -1 : Float.parseFloat(exameInput);
+				float exame = 0;
+				try {
+					exame = exameInput.isEmpty() ? -1 : Float.parseFloat(exameInput);
+				} catch (NumberFormatException e) {
+					System.out.println("Erro: Insira somente números");
+				}
 
 
 				Nota nNota = new Nota(alunoid, np1, np2, reposicao, exame);
